3da8993da58d17b964fd5f1076302f1f63caef3f
terry <terry_lee@yeah.net>
2021-01-14 23:07:12 +0800

    update

--- arithmetic/lru/sort/sort_test.go
+++ arithmetic/lru/sort/sort_test.go
@@ -21,3 +21,11 @@ func TestInsertionSort(t *testing.T) {
 		t.Log(v)
 	}
 }
+
+func TestSelectionSort(t *testing.T) {
+	a := []int{2, 5, 6, 7, 32, 6, 23, 4, 7, 8, 9, 4, 3, 5, 3}
+	sort.SelectionSort(a)
+	for _, v := range a {
+		t.Log(v)
+	}
+}
